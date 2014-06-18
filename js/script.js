986ec09d1c361865357f46724227587a4326a0d6
morlov <ormian@mail.ru>
2014-06-18 19:05:34 +0400

    refactored html templates
    better templating with jinja

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(function() {
-	$("#sortable").sortable({evert: true, 
+	$(".ranking-content").sortable({evert: true, 
 		update: function(event, ui) {
         var i = 1
         $(this).children().each(function(idx, val){
@@ -8,17 +8,18 @@ $(function() {
             	});	
 			}
 		});
-	$("#sortable").disableSelection();
+	$(".ranking-content").disableSelection();
 });
 
 $(document).ready(function()
 {	
-	var i = 1		
+	var i = 1;
+
 	$("#add-item").click(function(){
 		var item_text = $("#item-text").val()
 		if(item_text != '') {
 			var item_html = "<span class='item-rank'>" + (i++) + "</span> <span class='item-name'>" + item_text + "</span>";
-			$("<div class='ranking-item'>" + item_html + "</div>").appendTo("#sortable");
+			$("<div class='ranking-item'>" + item_html + "</div>").appendTo($(".ranking-content"));
 			$("#item-text").val("")
 		}
 	});
@@ -31,9 +32,10 @@ $(document).ready(function()
 			item_names: [],
 			item_contents: []
 		};
-		$("#new-ranking").submit(function(event) {
-			$("#sortable").children().each(function(idx, val){
-				ranking.item_ids.push($(this).find("#item-id").val());
+
+		$("#post-ranking").submit(function(event) {
+			$(".ranking-content").children().each(function(idx, val){
+				ranking.item_ids.push($(this).data("item-id"));
 				ranking.item_names.push($(this).find(".item-name").text());
 				ranking.item_contents.push($(this).find(".item-name").text());
             });	
