12bfb0a00486bec1948ec746a6ef4adc3d547fc3
Mikhail Orlov <mikhailorlov@192.168.1.36>
2015-08-16 17:44:51 +0400

    Front-end refactored

--- scripts/models/item.py
+++ scripts/models/item.py
@@ -10,7 +10,7 @@ class Item(Entity):
     name = db.StringProperty(required = False)
     content = db.TextProperty(required = False)
 
-	def save_item(name, content, creator):
+    def save_item(name, content, creator):
     	item = Item(name=name, content=content, creator=creator)
     	item.put()
     	return item.get_id()
--- scripts/models/user.py
+++ scripts/models/user.py
@@ -12,12 +12,12 @@ class User(Entity):
 	email = db.StringProperty(required = True)
 
 	def get_user_by_name(user_name):
-    	return User.all().filter("name =", user_name).get()
+		return User.all().filter("name =", user_name).get()
 
 	def get_user_by_email(email):
-    	return User.all().filter("email =",email).get()
+		return User.all().filter("email =",email).get()
 
 	def save_user(user_name, password, email):
-    	user = User(name=user_name, password=password, email=email)
+		user = User(name=user_name, password=password, email=email)
     	user.put()
     	return user.get_id()
